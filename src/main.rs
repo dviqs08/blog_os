4b51cb97d5c1bb5b72571cd80f88ff1cef47690e
dviqs08 <115051889+dviqs08@users.noreply.github.com>
2024-08-11 20:38:18 -0700

    no std and panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+// we have to define our own panic handler
+use core::panic::PanicInfo;
+
+// we have to disable the standard library
+fn main() {}
+
+// PanicInfo contains the file and line where the panic happened
+// and the optional panic message. THe function should never return,
+// that's what the exclamation point means.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
